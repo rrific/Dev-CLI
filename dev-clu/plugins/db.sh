0ea306a0a17c6d114086944ea49b8c3f918d501c
Patrick Petit <patrick@gitkraken.xyo.fr>
2018-11-22 10:41:50 +0100

    feat: updates way to connect to mysql for a dump
    fix: removes source
    
    chore: test

--- dev-clu/plugins/db.sh
+++ dev-clu/plugins/db.sh
@@ -21,7 +21,6 @@ esac
 
 case "$fn" in
     "db:backup")
-        source /usr/local/etc/config.sh
         dbname=$2
         
         # (1) set up all the mysqldump variables
@@ -32,25 +31,11 @@ case "$fn" in
         fi
 
         filename="$filepath/"`date +"%Y%m%d"`.sql
-        echo -e $filename
-        
-        # (2) in case you run this more than once a day, remove the previous version of the file
-        unalias rm     2> /dev/null
-        rm ${FILE}     2> /dev/null
-        rm ${FILE}.gz  2> /dev/null
-        
-        # (3) do the mysql database backup (dump)
-        
-        # use this command for a database server on a separate host:
-        #mysqldump --opt --protocol=TCP --user=${MYSQL_USER} --password=${PASS} --host=${DBSERVER} ${DATABASE} > ${FILE}
-        
+                
         # use this command for a database server on localhost. add other options if need be.
-        mysqldump -u "$MYSQL_USER" -p$MYSQL_PWD "$dbname" > "$filename"
-        
-        # (4) gzip the mysql database dump file
-        #gzip $filename
+        mysqldump --login-path=dump "$dbname" > "$filename"
         
         # (5) show the user the result
-        echo "$filename was created in folder $filepath"
+        echo "$dbname was backuped up as $filename"
     ;;
 esac
